b677e920d12aa224c398f4528ed3645a84827aec
Md.Asif Ur Rahman <45636891+asifurrahman01714@users.noreply.github.com>
2020-11-05 18:08:50 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function drawHands() {
-    const now = new Date();
-  
-    const s_deg = now.getSeconds() * 6;
-    $('.s').css('transform', `rotateZ(${s_deg}deg)`);
-  
-    const m_deg = now.getMinutes() * 6 + s_deg / 60;
-    $('.m').css('transform', `rotateZ(${m_deg}deg)`);
-  
-    const h_deg = now.getHours() * 30 + m_deg / 12;
-    $('.h').css('transform', `rotateZ(${h_deg}deg)`);
-  
-    setTimeout(drawHands, 1000);
-  }
-  
-  $(function () {
+function drawHands(){
+
+const now= new Date();
+
+const s_deg = now.getSeconds() * 6 ;
+$('.s').css('transform', `rotateZ(${s_deg}deg)`);
+
+const m_deg = now.getMinutes() * 6 + s_deg/60 ;
+$('.m').css('transform', `rotateZ(${m_deg}deg)`);
+
+const h_deg = now.getHours() * 30 + m_deg/12 ;
+$('.h').css('transform', `rotateZ(${h_deg}deg)`);
+
+
+setTimeout(drawHands, 1000);
+
+}
+
+$(function(){
     drawHands();
-  });
-  
+
+});
